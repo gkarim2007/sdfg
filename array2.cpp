e76edbb8a24a0113fe9b35e37e2cc8a902f97f7b
gkarim2007 <122677819+gkarim2007@users.noreply.github.com>
2023-02-08 18:55:19 +0000

    http

--- array2.cpp
+++ array2.cpp
@@ -1,81 +1,29 @@
 #include <string>
 #include <iostream>
 using namespace std;
-class Calc {
 
-
-public:
-    
-
-    virtual void plus(int a, int b) {
-        cout<<(a+b)<<"\n";
-    }
-
-    virtual void minus(int a, int b) {
-        cout<<(a-b)<<"\n";
-    }
-
-    // ~Calc() {}
-
-};
-
-class SuperCalc : public Calc {
-
- public:
-    void mult(int a, int b) { // умножение
-        cout<<(a*b)<<"\n";
-    }
-
-    void div(int a, int b) { // деление
-        cout<<(a/b)<<"\n";
-    }
-
-};
-
-class SaveCalc : public Calc { // сохраняет последнюю операцуию
-
-    
-
-    private:
-    std::string last_operation; // последняя выполненая операция
-
-    public:
-    void plus(int a, int b) {
-        cout<<(a+b)<<"\n";
-        this->last_operation=a+b;
-    }
-
-    void minus(int a, int b) {
-        cout<<(a-b)<<"\n";
-        this->last_operation=a-b;
-    }
-    std::string getLastOperation() {
-        return this->last_operation;
-        
-    }
-
-    // void plus() {...}
-    // void minus() {...}
-
-    // ~SaveCalc() {}
-
-};
 
 int main() {
-    Calc def_calc;
-    SuperCalc super_calc;
-    SaveCalc save_calc;
+    int a,b,o;
+    string g;
+    cin>>a;
+    cin>>g;
+    cin>>b;
 
-    def_calc.minus(100, 50);
-    def_calc.plus(5, 7);
-
-    super_calc.minus(10, 8);
-    super_calc.minus(50, 10);
-    super_calc.mult(7, 11);
-    super_calc.div(512, 16);
-
-    save_calc.minus(77, 8);
-    save_calc.plus(8, 60);
-    std::cout << save_calc.getLastOperation() << "\n";
-
+if (g=="+")
+{
+    cout<<(a+b);
+}
+if (g=="-")
+{
+    cout<<(a-b);
+}
+if (g=="*")
+{
+    cout<<(a*b);
+}
+if (g=="/")
+{
+    cout<<(a/b);
+}
 }
--- dz aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa/dz.cpp
+++ dz aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa/dz.cpp
@@ -1,72 +1,53 @@
 
 #include <iostream>
 #include <list>
-
-template <typename T>
-class List {
-    struct node {
-        T val;
-        node *next;
-    };
-
-    node *head;
-    int size;
-
- public:
-    List() {}
-    List(const List &other) {}
-
-    List(int size, T val) {
-        this->size = 0;
-        int tmp = size;
-        while (tmp != 0) {
-            add_end(val);
-            --tmp;
-        }
+using namespace std;
+#include <fstream>
+class aaa {
+    string line1=0;
+    string line2=0;
+    std::ifstream in ("D:\\name.txt");
+    
+    if (in.is_open())
+    {
+        cout << "Hello World!" << std::endl;
+    }
+    else{
+        cout<<"aaa";
+    }
+    std::ifstream oout("D:\\aaa.txt");
+    
+    if (oout.is_open())
+    {
+        cout << "Hello World!" << std::endl;
+    }
+    else{
+        cout<<"aaa";
+    }
+    getline(in, line1);
+    getline(oout, line2);
+    if(line1==line2)
+    {
+        cout<<"a";
     }
 
-    void print_list() {
-        for (node *i = head; i != nullptr; i = i->next) {
-            std::cout << i->val << " ";
-        }
-        std::cout << "\n";
-    }
-
-    void add_end(const T &val) {
-        if (head == nullptr) {
-            head = new node {val, nullptr};
-        } else {
-            node *i = head;
-            while (i->next != nullptr) {
-                i = i->next;
-            }
-            i->next = new node {val, nullptr};
-        }
-        this->size += 1;
-    }
-    void add_golova(const T &val) {
-        if (head == nullptr) {
-            head = new node {val, nullptr};
-        } else {
-            node *i = head;
-            while (i->next != head) {
-                i = i->next;
-            }
-            i->next = new node {val, head};
-        }
-        this->size += 1;
-    }
 };
 
 
 
 int main() {
     
-    List<int> my_list(7, 4);
-    my_list.print_list();
-    my_list.add_end(6);
-    my_list.print_list();
-    my_list.add_golova(6);
-    my_list.print_list();
+    std::ofstream out; 
+ 
+
+    out.open("name.txt"); 
+    
+    if (out.is_open())
+    {
+        cout << "Hello World!" << std::endl;
+    }
+    else{
+        cout<<"aaa";
+    }
     return 0;
 }
