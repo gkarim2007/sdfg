3268150cbe5f48fa23d7dce5214e0212aeada00c
gkarim2007 <122677819+gkarim2007@users.noreply.github.com>
2023-02-11 13:24:17 +0000

    SDFG

--- dz aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa/dz.cpp
+++ dz aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa/dz.cpp
@@ -1,62 +1,74 @@
-
+#include <string>
 #include <iostream>
-#include <list>
 using namespace std;
-#include <fstream>
-class aaa {
-    string line2=0;
-    
-    ifstream in ("name.txt");
-    std::ofstream out;        
-    out.open("name.txt");
-    void aaa()
+class Parent {
+ public:
+    string name; 
+    void imauznat(string ima)
     {
-        if (in.is_open())
-    {
-        cout << "Hello World!" << std::endl;
+        this->name=ima;
     }
-    else{
-        cout<<"aaa";
-    }
-    
-    
-    }
-    
-    string bratvse()
-    {
-        while (getline(oout, line2);)
-    {
-        cout<<line<<"\n";
-    }
-    }
-    
-    string bratliniju(chislo)
-    {
-        for (int i = 0; i != chislo; ++i)
-        {
-            getline(oout, line2);
-            if (i==chislo)
-            {
-                cout<<line2;
-            }
-            
-        }
-    }     
-string datstroka(chislo)
-    {
-        out<<"ROUDENR";
-    }     
-    string datstroka(chislo)
-    {
-        out<<"ROUDENR";
-    }     
+
 };
 
+class zver1 : public Parent {
+ private:
+    string vid="koshka";
+ public:
+void imagovorit()
+{
+    cout<<this->name<<'\n';
+}
+void vidgovorit()
+{
+    cout<<this->vid<<'\n';
+}
 
+};
+class zver2 : public Parent {
+ private:
+    string vid="sobaka";
+ public:
+void imagovorit()
+{
+    cout<<this->name<<'\n';
+}
+void vidgovorit()
+{
+    cout<<this->vid<<'\n';
+}
 
+};
+class zver3 : public Parent {
+ private:
+    string vid="popugai";
+ public:
+void imagovorit()
+{
+    cout<<this->name<<'\n';
+}
+void vidgovorit()
+{
+    cout<<this->vid<<'\n';
+}
+
+};
 int main() {
     
-    aaa asasa;
-    asasa.bratliniju;
+    Parent *test1 = new zver1();
+    ((zver1*)test1)->imauznat("ROUDENR");
+    ((zver1*)test1)->imagovorit();
+((zver1*)test1)->vidgovorit();
+    delete test1;
+    Parent *test2 = new zver2();
+    ((zver2*)test2)->imauznat("SDFG");
+    ((zver2*)test2)->imagovorit();
+((zver2*)test2)->vidgovorit();
+    delete test2;
+    Parent *test3 = new zver3();
+    ((zver3*)test3)->imauznat("AAAAAA");
+    ((zver3*)test3)->imagovorit();
+((zver3*)test3)->vidgovorit();
+    delete test3;
     return 0;
 }
