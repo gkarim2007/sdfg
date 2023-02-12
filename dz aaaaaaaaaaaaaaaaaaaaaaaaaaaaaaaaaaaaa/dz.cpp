a1f89b75b3ee3ddb1d78e0ed490487601ccf3503
gkarim2007 <122677819+gkarim2007@users.noreply.github.com>
2023-02-12 16:27:50 +0000

    qwer

--- dz aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa/dz.cpp
+++ dz aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa/dz.cpp
@@ -2,73 +2,29 @@
 #include <iostream>
 using namespace std;
 class Parent {
+    int n;
  public:
-    string name; 
-    void imauznat(string ima)
+    
+    
+    void vvod()
     {
-        this->name=ima;
+        cin>>this->n;
+    string *stroka=new string[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin>>stroka[i];
+        }
+        for (int i = 0; i < n; i++)
+        {
+            cout<<stroka[i];
+        }
     }
-
 };
 
-class zver1 : public Parent {
- private:
-    string vid="koshka";
- public:
-void imagovorit()
-{
-    cout<<this->name<<'\n';
-}
-void vidgovorit()
-{
-    cout<<this->vid<<'\n';
-}
-
-};
-class zver2 : public Parent {
- private:
-    string vid="sobaka";
- public:
-void imagovorit()
-{
-    cout<<this->name<<'\n';
-}
-void vidgovorit()
-{
-    cout<<this->vid<<'\n';
-}
-
-};
-class zver3 : public Parent {
- private:
-    string vid="popugai";
- public:
-void imagovorit()
-{
-    cout<<this->name<<'\n';
-}
-void vidgovorit()
-{
-    cout<<this->vid<<'\n';
-}
-
-};
 int main() {
     
-    Parent *test1 = new zver1();
-    ((zver1*)test1)->imauznat("ROUDENR");
-    ((zver1*)test1)->imagovorit();
-((zver1*)test1)->vidgovorit();
-    delete test1;
-    Parent *test2 = new zver2();
-    ((zver2*)test2)->imauznat("SDFG");
-    ((zver2*)test2)->imagovorit();
-((zver2*)test2)->vidgovorit();
-    delete test2;
-    Parent *test3 = new zver3();
-    ((zver3*)test3)->imauznat("AAAAAA");
-    ((zver3*)test3)->imagovorit();
-((zver3*)test3)->vidgovorit();
-    delete test3;
+    Parent test1;
+    test1.vvod();
+    
     return 0;
 }
