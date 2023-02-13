f0b06272a223ad61e847b8ddb89ea25653f64d5e
gkarim2007 <122677819+gkarim2007@users.noreply.github.com>
2023-02-13 19:06:04 +0000

    sdfg

--- array.cpp
+++ array.cpp
@@ -1,68 +1,87 @@
-
 #include <iostream>
-#include <list>
+#include <string>
+using namespace std;
 
-template <typename T>
-class List {
-    struct node {
-        T val;
-        node *next;
-    };
-
-    node *head;
-    int size;
-
- public:
-    List() {}
-    List(const List &other) {}
-
-    List(int size, T val) {
-        this->size = 0;
-        int tmp = size;
-        while (tmp != 0) {
-            add_end(val);
-            --tmp;
+template <typename t>
+class massiv {
+    int n;
+    t *arrei;
+    public:
+    massiv ()
+    {
+        cin>>n;
+        arrei=new t [n*2];
+        for (int i = 0; i < n; i++)
+        {
+            cin>>arrei[i];
+            cout<<arrei[i]<<" ";
         }
+            cout<<"\n";
     }
+    void dobavitnazad()
+    {
+        cin>>arrei[n];
+        n++;
 
-    void print_list() {
-        for (node *i = head; i != nullptr; i = i->next) {
-            std::cout << i->val << " ";
+        for (int i = 0; i < n; i++)
+        {
+            cout<<arrei[i]<<" ";
         }
-        std::cout << "\n";
+            cout<<"\n";
     }
-
-    void add_end(const T &val) {
-        if (head == nullptr) {
-            head = new node {val, nullptr};
-        } else {
-            node *i = head;
-            while (i->next != nullptr) {
-                i = i->next;
-            }
-            i->next = new node {val, nullptr};
+    void dobavitvpered()
+    {
+        n++;
+        for (int i = n; i > 0; i--)
+        {
+            arrei[i]=arrei[i-1];
+            
         }
-        this->size += 1;
+        
+        cin>>arrei[0];
+        for (int i = 0; i < n; i++)
+        {
+            
+            cout<<arrei[i]<<" ";
+        }
+            cout<<"\n";
+    }
+    void uniztozitvpered()
+    {
+        n--;
+        
+        for (int i = 0; i < n; i++)
+        {
+            
+            cout<<arrei[i]<<" ";
+        }
+            cout<<"\n";
+    }
+    void uniztozitnazad()
+    {
+        
+        for (int i = 0; i < n; i++)
+        {
+            arrei[i]=arrei[i+1];
+        }
+        n--;
+        
+        for (int i = 0; i < n; i++)
+        {
+            
+            cout<<arrei[i]<<" ";
+        }
+            cout<<"\n";
     }
 };
-
-void print_list(const std::list<int> &list) {
-
-    std::cout << "Size = " << list.size() << "\n";
-
-    for (std::list<int>::const_iterator i = list.begin();
-         i != list.end();
-         ++i) {
-            std::cout << *i << " ";
-         }
-        std::cout << "\n";
-}
-
 int main() {
-    // std::list<int> test_list(3, 6);
-    // print_list(test_list);
+    massiv<int> aaa;
+    aaa.dobavitnazad();
+    aaa.dobavitvpered();
+    aaa.uniztozitnazad();
 
-    List<int> my_list(7, 4);
-    my_list.print_list();
+    aaa.uniztozitvpered();
+    aaa.uniztozitvpered();
+    aaa.uniztozitvpered();
     return 0;
 }
--- array2.cpp
+++ array2.cpp
@@ -3,27 +3,36 @@
 using namespace std;
 
 
-int main() {
-    int a,b,o;
-    string g;
-    cin>>a;
-    cin>>g;
-    cin>>b;
-
-if (g=="+")
+int main()
 {
-    cout<<(a+b);
-}
-if (g=="-")
-{
-    cout<<(a-b);
-}
-if (g=="*")
-{
-    cout<<(a*b);
-}
-if (g=="/")
-{
-    cout<<(a/b);
-}
+    int n,m;
+    cin>>n>>m;
+    int sum=0;
+    int** Mas = new int *[n];
+    for (int i = 0; i < n; i++)
+        Mas[i] = new int [m];
+ 
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < m; j++)
+        {
+            cin>>Mas[i][j];
+        }
+ 
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+            cout<<Mas[i][j]<<" ";
+        cout<<"\n";
+    }
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            sum=sum+Mas[i][j];
+        }
+        cout<<sum/m;
+        sum=0;
+    }
+    
+    return 0;
 }
