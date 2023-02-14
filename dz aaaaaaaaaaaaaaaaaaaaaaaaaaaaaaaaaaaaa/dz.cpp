672477aceebf8f58715f98ca8ef18499f97edc5a
gkarim2007 <122677819+gkarim2007@users.noreply.github.com>
2023-02-14 16:09:02 +0000

    qwer

--- dz aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa/dz.cpp
+++ dz aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa/dz.cpp
@@ -1,30 +1,60 @@
 #include <string>
 #include <iostream>
 using namespace std;
-class Parent {
-    int n;
+class dvigatel {
+    int mosch;
+    int ves;
+    string nazvanije;
  public:
-    
-    
-    void vvod()
+    dvigatel()
     {
-        cin>>this->n;
-    string *stroka=new string[n];
-        for (int i = 0; i < n; i++)
-        {
-            cin>>stroka[i];
-        }
-        for (int i = 0; i < n; i++)
-        {
-            cout<<stroka[i];
-        }
+        this->mosch=999;
+        this->ves=1;
+        this->nazvanije="RTX3600";
+    }
+    void getmosch()
+    {
+        cout<<"mosch "<<mosch<<"\n";
+    }
+    void getves()
+    {
+        cout<<"ves "<<ves<<"\n";
+    }
+    void getnazvanije()
+    {
+        cout<<"nazvanije "<<nazvanije<<"\n";
+    }
+    void getvse()
+    {
+        getmosch();
+        getves();
+        getnazvanije();
     }
 };
-
+class mashina : dvigatel{
+    int babki;
+    string cvet;
+    string Nazvanije;
+ public:
+    mashina()
+    {
+        this->babki=99999999;
+        this->cvet="baklajan";
+        this->Nazvanije="mersebenzdens";
+    }
+    void getvsevse()
+    {
+        cout<<"babki"<<babki<<"\n";
+        cout<<"cvet"<<cvet<<"\n";
+        cout<<"Nazvanije"<<Nazvanije<<"\n";
+        cout<<"dvigatel"<<"\n";
+        getvse();
+    }
+};
 int main() {
     
-    Parent test1;
-    test1.vvod();
+    mashina test1;
+    test1.getvsevse();
     
     return 0;
 }
