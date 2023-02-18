c6371d42e6c6cdac260af559933ca9bee57d62f2
gkarim2007 <122677819+gkarim2007@users.noreply.github.com>
2023-02-18 17:49:27 +0000

    dhytdhyttg

--- dz aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa/dz.cpp
+++ dz aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa/dz.cpp
@@ -1,60 +1,17 @@
 #include <string>
 #include <iostream>
 using namespace std;
-class dvigatel {
-    int mosch;
-    int ves;
-    string nazvanije;
- public:
-    dvigatel()
-    {
-        this->mosch=999;
-        this->ves=1;
-        this->nazvanije="RTX3600";
-    }
-    void getmosch()
-    {
-        cout<<"mosch "<<mosch<<"\n";
-    }
-    void getves()
-    {
-        cout<<"ves "<<ves<<"\n";
-    }
-    void getnazvanije()
-    {
-        cout<<"nazvanije "<<nazvanije<<"\n";
-    }
-    void getvse()
-    {
-        getmosch();
-        getves();
-        getnazvanije();
-    }
+
+class student
+{
+    string ima="sdfg";
+    int ocenki[5]{5,5,5,5,5};
 };
-class mashina : dvigatel{
-    int babki;
-    string cvet;
-    string Nazvanije;
- public:
-    mashina()
-    {
-        this->babki=99999999;
-        this->cvet="baklajan";
-        this->Nazvanije="mersebenzdens";
-    }
-    void getvsevse()
-    {
-        cout<<"babki"<<babki<<"\n";
-        cout<<"cvet"<<cvet<<"\n";
-        cout<<"Nazvanije"<<Nazvanije<<"\n";
-        cout<<"dvigatel"<<"\n";
-        getvse();
-    }
+class aspirant : student
+{
+    string kandidatskajarabota="aaaaaaaaaaaaa";
 };
 int main() {
-    
-    mashina test1;
-    test1.getvsevse();
     
     return 0;
 }
