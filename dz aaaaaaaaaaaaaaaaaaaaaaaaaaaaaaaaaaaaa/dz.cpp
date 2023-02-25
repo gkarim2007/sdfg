09bf7c8e9df7cd013d4b2314c19f9cdb86a87632
gkarim2007 <122677819+gkarim2007@users.noreply.github.com>
2023-02-25 14:30:39 +0000

    SDFG

--- dz aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa/dz.cpp
+++ dz aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa/dz.cpp
@@ -1,46 +1,67 @@
 #include <iostream>
-
+#include <string>
 using namespace std;
+class spravochnik
+{
+    string a;
+    int b;
+    string c;
+    string nazvanije="ROUDENR";
+    int telefon=4258997098;
+    string vladelec="SDFG";
+    string nazvanije2="ROUDENR2";
+    int telefon2=4258997082;
+    string vladelec2="SDFG2";
+    public:
+    void iskatponazvaniju()
+    {
+        cin>>a;
+        if (a==nazvanije)
+        {
+            cout<<telefon<<"\n";
+            cout<<vladelec;
+        }
+        if (a==nazvanije2)
+        {
+            cout<<telefon2<<"\n";
+            cout<<vladelec2;
+        }
+    }
+    void iskatpotelefonu()
+    {
+        cin>>b;
+        if (b==telefon)
+        {
+            cout<<nazvanije<<"\n";
+            cout<<vladelec;
+        }
+        if (b==telefon2)
+        {
+            cout<<nazvanije2<<"\n";
+            cout<<vladelec2;
+        }
+    }
+    void iskatpovladelcu()
+    {
+        cin>> c;
+        if (c==vladelec)
+        {
+            cout<<telefon<<"\n";
+            cout<<nazvanije;
+        }
+        if (c==vladelec2)
+        {
+            cout<<telefon2<<"\n";
+            cout<<nazvanije2;
+        }
+    }
+};
 
 int main()
 {
-    int sovpajenija=0;
-    int n;
-    cin >>n;
-    int *a= new int[n];
-    int m;
-    cin >>m;
-    int *b= new int[m];
-    for(int i=0;i<m;i++)
-    {
-        cin>>b[i];
-    }
-    for(int i=0;i<n;i++)
-    {
-        cin>>a[i];
-    }
-    for(int j=0;j<n;j++)
-    {
-        for(int i=0;i<m;i++)
-        {
-            if(a[j]!=b[i])
-            {
-                sovpajenija=sovpajenija+ 1;
-            }
-        }
-    }
-    int *c= new int[sovpajenija];
-    for(int j=0;j<n;j++)
-    {
-        for(int i=0;i<m;i++)
-        {
-            if(a[j]!=b[i])
-            {
-                c[j]=b[i];
-                
-            }
-        }
-        cout<<c[j];
-    }
+    spravochnik aaa;
+    aaa.iskatponazvaniju();
+    aaa.iskatpotelefonu();
+    aaa.iskatpovladelcu();
     return 0;
 }
